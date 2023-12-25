a51912d59640aad66906e8a140675defda59a4f7
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 20:36:02 +0500

    variables in rust

---
+++ Basics/practice1.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    let a = 5;
+    println!("{}", a);
+    println!("can not change value of variable in rust so changing it as by keyboard mut to make it mutable");
+    let mut b = 6;
+    b = 4;
+    println!("{} after changing value", b);
+}
