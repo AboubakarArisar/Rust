40d6ef1d9fb504796131230cab40477bc80c2c41
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 21:06:12 +0500

    positional arguments in rust

---
+++ Basics/positional-Arguments.rs
@@ -0,0 +1,6 @@
+//in rust positional arguments for printing values on any place according to their positions
+
+
+fn main() {
+    println!("Showcase your codes with the world on  {0}.  {0} is a very {1} platform", "Educative", "interactive");
+}
