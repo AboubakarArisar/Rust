ccf368cd366ba6aa0f2daa160525eba09f2c1e67
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 23:26:12 +0500

    Airthmetric operators in rust

---
+++ Basics/operaters.rs
@@ -0,0 +1,14 @@
+//Binary operators: that works on 2 operands
+//Airthmetic operators
+
+fn main() {
+    let a = 4;
+    let b = 3;
+    
+    println!("Operand 1:{}, Operand 2:{}", a , b);
+    println!("Addition:{}", a + b);
+    println!("Subtraction:{}", a - b);
+    println!("Multiplication:{}", a * b);
+    println!("Division:{}", a / b);
+    println!("Modulus:{}", a % b);
+}
