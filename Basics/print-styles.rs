3a12090273a3a0b9e9d9e85925db978a879226a5
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 21:41:00 +0500

    printing styles in rust

---
+++ Basics/print-styles.rs
@@ -0,0 +1,17 @@
+//There are 4 types of print styles
+
+
+fn main(){
+//print!() for printing on same line
+print!("on same line");
+
+//println!() for printing on next line 
+println!("on next line");
+
+//eprint!() for printing as error 
+eprint!("printing as error");
+
+//eprintln!() for printing as error on next line
+
+eprintln!("printing as error on next line");
+}
