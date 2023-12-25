ee403148710c4fb0d9e6f6dfb319c500b436927d
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 21:03:10 +0500

    placeholders in rust

---
+++ Basics/placeholder.rs
@@ -0,0 +1,9 @@
+//in rust values are given in placeholders you can use multiple placeholders also , every placeholder contains a value
+
+fn main(){
+  println!("Number is {}", 8);
+  println!("This is me {} pushing my rust code on {}","Aboubakar","Github");
+}
+
+
+//That's how you print values in macro (println!) 
