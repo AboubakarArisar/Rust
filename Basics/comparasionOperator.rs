50524747032126379b62d44b2a8628830ef7a3b4
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 23:32:08 +0500

    Comparison Operator in rust

---
+++ Basics/comparasionOperator.rs
@@ -0,0 +1,14 @@
+//Comparasion operator in rust
+
+
+fn main() {
+    let a = 2;
+    let b = 3;
+    println!("Operand 1:{}, Operand 2:{}", a , b);
+    println!("a > b:{}", a > b);
+    println!("a < b:{}", a < b);
+    println!("a >= b:{}", a >= b);
+    println!("a <= b:{}", a <= b);
+    println!("a == b:{}", a == b);
+    println!("a != b:{}", a != b);
+}
