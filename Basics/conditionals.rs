216f3cfde1fd2adea85122aa160a8c764a164ae7
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-26 00:00:56 +0500

    If else in Rust

---
+++ Basics/conditionals.rs
@@ -0,0 +1,16 @@
+//if else if and else in Rust
+
+fn main() {
+      //define a variable 
+      let learn_language="Rust";
+      // if..elseif..else  
+      if learn_language == "Rust" { 
+         println!("You are learning Rust language!");
+      }
+      else if learn_language == "Java" { 
+         println!("You are learning Java language!");
+      }
+      else {
+         println!("You are learning some other language!");
+      } 
+}
