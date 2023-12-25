87e199b5ad458ca35517c8c96470fe961df90e6d
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 23:20:14 +0500

    Tupels in rust

---
+++ Basics/Tuples.rs
@@ -0,0 +1,5 @@
+fn test() {
+    // Write code here!
+    let persons =("Alex",21,"Abe",22,"Anna",23);
+    print!("{}:{}, {}:{}, {}:{}",persons.0,persons.1,persons.2,persons.3,persons.4,persons.5);
+}
