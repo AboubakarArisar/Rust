5de719bce26fb7acbff79eddb49238798e988788
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 21:14:05 +0500

    placeholder traits in rust

---
+++ Basics/placeholder-trait.rs
@@ -0,0 +1,5 @@
+//if you want to convert number to hexadecimal , binary or orctal u can do it {:x} for hexa , {:b} for binary and {:o} for octal
+
+fn main() {
+    println!("Number : 10 \nBinary:{:b} \nHexadecimal:{:x} \nOctal:{:o}", 10, 10, 10);
+}
