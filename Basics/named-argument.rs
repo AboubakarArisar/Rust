b71398e01f3a56fd6b56e2ef02e2aeca9bb1e819
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-25 21:09:22 +0500

    named arguments in rus

---
+++ Basics/named-argument.rs
@@ -0,0 +1,6 @@
+//A placeholder can take a named argument and assign it a value explicitly.
+//It does this by specifying a name within the placeholder and assign that name the value to be inserted in the string.
+
+fn main() {
+    println!("{company} provides {kind} features", company = "Github", kind = "interactive");
+}
