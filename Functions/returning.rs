9c396ff92ac1325ed5ce0debf1e23cf4a0022514
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-28 00:49:58 +0500

    functions returning values in rust

---
+++ Functions/returning.rs
@@ -0,0 +1,11 @@
+fn square(n:i32)->i32{
+  println!("The value of n inside function : {}", n);
+  let m = n * n;
+  m // return the square of the number n
+}  
+fn main() {
+  let  n = 4;
+  println!("The value of n before function call : {}", n);
+  println!("Invoke Function");
+  println!("\nOutput : {}",square(n));
+}
