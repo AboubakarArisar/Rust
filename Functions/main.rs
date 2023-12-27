6d3bf03047522e211fa0b7523424bc5fc4f55637
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-28 00:44:25 +0500

    main function in rust

---
+++ Functions/main.rs
@@ -0,0 +1,6 @@
+fn main(){
+//program starts from here
+
+//The simplest possible function that we have studied so far is the main function that is declared with fn keyword. This is where the program execution starts.
+//However, it is possible to define a user-defined function.
+}
