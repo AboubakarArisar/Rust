d71fd84600ed08af3486e78b887df7113b06dec7
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-28 00:40:19 +0500

    Right angle triangle in rut

---
+++ Loops/rightAngleTriangle.rs
@@ -0,0 +1,15 @@
+fn test(n:i32) {
+    // define a nested for loop
+    for i in 0..n { //outer loop
+        for j in 0..i + 1 { // inner loop
+            print!("&");
+    }
+    println!("");
+    }
+}
+fn main(){
+    println!("Right angled triangle when n = 5 ");
+    test(5);
+    println!("Right angled triangle when n = 6 ");
+    test(6);
+}
