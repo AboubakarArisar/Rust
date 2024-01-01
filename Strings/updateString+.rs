429d0bd29a8cfb3124d389c38bb08bd08db40c30
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-01-02 00:51:21 +0500

    update string using + in rust

---
+++ Strings/updateString+.rs
@@ -0,0 +1,10 @@
+
+fn main(){
+   // define a String object 
+   let course = "Rust".to_string();
+   // define a String object
+   let course_type = " beginner course".to_string();
+   // concatenate using the + operator
+   let result = course + &course_type;
+   println!("{}", result);
+}
