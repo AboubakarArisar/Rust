815f4aac7260aa803f22b201280f598ee4adff41
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-01-02 00:48:47 +0500

    updating string in rust
    Update string by adding a single character at run time using push method in rust

---
+++ Strings/updateString.rs
@@ -0,0 +1,7 @@
+fn main() {
+  // define a String object
+  let mut course = String::from("Rus");
+  // push a character
+  course.push('t');
+  println!("This is a beginner course in {}.", course);
+}
